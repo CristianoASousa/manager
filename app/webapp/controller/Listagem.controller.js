a0f3b6e15202b2cbcba4b6cea1a6986e6f36fc12
Iago Baldani <iagobaldanialmeida@hotmail.com>
2021-05-26 00:21:36 +0000

    Ajustes gerais

--- app/webapp/controller/Listagem.controller.js
+++ app/webapp/controller/Listagem.controller.js
@@ -1,33 +1,65 @@
 sap.ui.define([
-    "./BaseController"
+    "./BaseController",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageBox",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
 	/**
 	 * @param {typeof sap.ui.core.mvc.Controller} Controller
 	 */
-    function (BaseController) {
+    function (BaseController, JSONModel, MessageBox, Filter, FilterOperator) {
         "use strict";
 
         return BaseController.extend("manager.app.controller.Listagem", {
             onInit: function () {
-                var emp = {
-                    "name": "Cristiano",
-                    "birthDate": "1994-01-01",
-                    "email": "cristianossn7@gmail.com",
-                    "groupRisk": false,
-                    "livesRiskGroup": true,
-                    "publicTransport": true,
-                    "hourPublicTransport": true,
-                    "environmentEquipment": true,
-                    "inHomeOffice": null,
-                    "vaccinated": false,
-                    "score": 70,
-                    "gotCOVID": false,
-                    "activeCase": false,
-                    "contaminationDate": "2020-02-06",
-                    "department_ID": 1
+                this.getRouter().getRoute("Listagem").attachPatternMatched(this.handleRouteMatched, this);
+                
+                var Employee = {
+                    "name": "Iago Baldani",
+                    "email": "iagobaldani@gmail.com",
+                    "department": {
+                        "name": "Projetos"
+                    },
+                    "score": 40,
+                    "inHomeOffice": true
+                };
+
+                this.getView().setModel(new JSONModel(Employee), "Employees");
+
+            },
+
+            handleRouteMatched: async function(){
+                var that = this; 
+                
+                /* this.getView().setBusy(true);
+
+                 await $.ajax({
+                    "url":"/api/main/Employees",
+                    "method":"GET",
+                    success(data){
+                        that.getView().setModel(new JSONModel(data), "Employees");
+                        that.getView().setBusy(false);
+                    },
+                    error(){
+                        MessageBox.error("Não foi possível gerar a lista. Tente novamente mais tarde.");
+                    }
+                }); */
+            },
+
+            onSearch: function(oEvent){
+                var aFilters = [];
+                var sQuery = oEvent.getSource().getValue();
+                if (sQuery && sQuery.length > 0) {
+                    var filter = new Filter("email", FilterOperator.Contains, sQuery);
+                    aFilters.push(filter);
                 }
 
-                
+                var oList = this.byId("tableEmployees");
+                var oBinding = oList.getBinding("items");
+                oBinding.filter(aFilters, "Application");
             }
+
+
         });
     });
